c707c897ac661adf3bf9d33a6cadef1dc0d72697
bara-codes <198811843+bara-codes@users.noreply.github.com>
2025-10-06 00:01:45 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -11,19 +11,19 @@ public class FizzBuzz
      */
     public static void main(String[] args)
     {
-        for(int j=0; j<=100; j++)
+        for(int j=1; j<=100; j++)
         {
-            if (j % 3 == 0 && j>=3)
+            if (j % 3 == 0 && j % 5 == 0)
             {
-                System.out.println("Fizz");
+                System.out.println("FizzBuzz");
             }
-            else if (j % 5 == 0 && j>=5)
+            else if (j % 5 == 0 )
             {
                 System.out.println("Buzz");
             }
-            else if (j % 3 == 0 && j % 5 == 0 && j>=3)
+            else if (j % 3 == 0)
             {
-                System.out.println("FizzBuzz");
+                System.out.println("Fizz");
             }
             else
             {
